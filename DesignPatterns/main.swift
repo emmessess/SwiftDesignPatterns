f68cdd3e0ba7155fed26d8cc7d63e7af219d5a35
MMQ <sadiq.siddiqui@hotmail.com>
2023-02-14 13:10:41 +0500

    Revert "Abstract Factory Pattern"
    This reverts commit 735e42c99192ab3210e55f9ba1fad4a6679e4ef0.

--- DesignPatterns/main.swift
+++ DesignPatterns/main.swift
@@ -6,26 +6,6 @@
 //
 
 import Foundation
-let apple : Brand = .apple
-let google : Brand = .google
 
-let stevePreference = Preference(brand: apple)
-let sundarPreference = Preference(brand: google)
-
-let factory1 = stevePreference.getPreferedFactory()
-let factory2 = sundarPreference.getPreferedFactory()
-
-let phone1 = factory1.createSmartPhone()
-let phone2 = factory2.createSmartPhone()
-
-let digitalWatch1 = factory1.createSmartWatch()
-let digitalWatch2 = factory2.createSmartWatch()
-
-phone1.sendMessage()
-phone2.sendMessage()
-
-digitalWatch1.navigate()
-digitalWatch2.navigate()
-
-
+print("Hello, World!")
 
